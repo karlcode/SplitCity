a31d5585fefcb21d9f0113bc24e9ef75b3bba071
karlcode <karl.l1@hotmail.com>
2019-01-30 23:05:09 +1100

    Image.network playing around

--- lib/main.dart
+++ lib/main.dart
@@ -20,15 +20,50 @@ class FirstScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('First Screen'),
+        title: Text('First sd'),
       ),
       body: Center(
         child: RaisedButton(
-          child: Text('Launch screen'),
+          child: Text('Send it'),
           onPressed: () {
             // Navigate to the second screen using a named route
             Navigator.pushNamed(context, '/second');
           },
+        ),
+      ),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the Drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Drawer Header'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('Home'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pushNamed(context, '/');
+              },
+            ),
+            ListTile(
+              title: Text('Second Screen'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pushNamed(context, '/second');
+              },
+            ),
+          ],
         ),
       ),
     );
@@ -43,13 +78,8 @@ class SecondScreen extends StatelessWidget {
         title: Text("Second Screen"),
       ),
       body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            // Navigate back to the first screen by popping the current route
-            // off the stack
-            Navigator.pop(context);
-          },
-          child: Text('Go back!'),
+        child: Image.network(
+        'https://picsum.photos/250?image=9',
         ),
       ),
     );
