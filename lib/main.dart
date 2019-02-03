e98742d329cbf266f4d66f43fb8fa4d458b23355
karlcode <karl.l1@hotmail.com>
2019-02-03 22:32:49 +1100

    Udacity tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -1,112 +1,33 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    theme: ThemeData(
-      // Define the default Brightness and Colors
-      brightness: Brightness.dark,
-      primaryColor: Colors.white,
-      accentColor: Colors.white,
-
-      // Define the default Font Family
-      fontFamily: 'Montserrat',
-
-      // Define the default TextTheme. Use this to specify the default
-      // text styling for headlines, titles, bodies of text, and more.
-
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Hello'),
+      ),
+      body: HelloRectangle(),
     ),
-    title: 'Named Routes',
-    // Start the app with the "/" named route. In our case, the app will start
-    // on the HomeScreen Widget
-    // initialRoute: '/',
-    // routes: {
-    //   // When we navigate to the "/" route, build the HomeScreen Widget
-    //   '/': (context) => HomeScreen(),
-    //   // When we navigate to the "/second" route, build the SecondScreen Widget
-    //   '/second': (context) => SecondScreen(),
-    // },
-    home: HomeScreen(),
   ));
 }
 
-class HomeScreen extends StatelessWidget {
+class HelloRectangle extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Text('Home'),
-        elevation: 5,
-        centerTitle: true,
-        textTheme: TextTheme(
-            title: TextStyle(
-          color: Colors.black,
-          fontSize: 20.0,
-        )),
-      ),
-      body: Center(
+    return Container(
+      color: Colors.pinkAccent,
+      height: 400.0,
+      width: 300.0,
+      child: Center(
         child: RaisedButton(
-          child: RandomWords(),
-          onPressed: () {
-            // Navigate to the second screen using a named route
-            Navigator.pushNamed(context, '/second');
-          },
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-          items: <BottomNavigationBarItem>[
-            BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
-            BottomNavigationBarItem(icon: Icon(Icons.business), title: Text('Business')),
-            BottomNavigationBarItem(icon: Icon(Icons.school), title: Text('School')),
-          ],
-          fixedColor: Colors.deepPurple,
+          child: Text('Me'),
+          onPressed: () {print('I was tapped');},
         )
-    );
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  RandomWordsState createState() => new RandomWordsState();
-}
-
-class RandomWordsState extends State<RandomWords> {
-  @override
-  final _suggestions = <WordPair>[]; //enforces privacy
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        centerTitle: true,
-        title: Text('Name'),
-        backgroundColor: Colors.white,
-      ),
-      body: _buildSuggestions(),
-    );
-  }
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: /*1*/ (context, i) {
-          if (i.isOdd) return Divider(); /*2*/
-
-          final index = i ~/ 2; /*3*/
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair) {
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
       ),
     );
   }
