7ee578d58fa2e0da21b228c89b3d39b8fd4131d9
karlcode <karl.l1@hotmail.com>
2019-05-12 20:21:48 +1000

    Playing around with Flutter

--- lib/components/home_page.dart
+++ lib/components/home_page.dart
@@ -4,10 +4,6 @@
 
 import 'package:flutter/material.dart';
 
-import 'package:myapp/components/category.dart';
-import 'package:myapp/components/unit.dart';
-
-final _backgroundColor = Colors.green;
 
 /// Category Route (screen).
 ///
@@ -29,99 +25,59 @@ final _backgroundColor = Colors.green;
 // }
 
 class HomePage extends StatelessWidget {
-  // final String color;
-  // const CategoryRoute({
-  //   this.color
-  // });
-
-  // TODO: Create State object for the CategoryRoute
-
-  // @override
-  // createState() => _CategoryRouteState();
-
-  static const _categoryNames = <String>[
-    'Length',
-    'Area',
-    'Volume',
-    'Mass',
-    'Time',
-    'Digital Storage',
-    'Energy',
-    'Currency',
-  ];
-
-  static const _baseColors = <Color>[
-    Colors.teal,
-    Colors.amberAccent,
-    Colors.pinkAccent,
-    Colors.blueAccent,
-    Colors.yellow,
-    Colors.greenAccent,
-    Colors.purpleAccent,
-    Colors.blue,
-  ];
-
-  /// Makes the correct number of rows for the list view.
-  ///
-  /// For portrait, we use a [ListView].
-  Widget _buildCategoryWidgets(List<Widget> categories) {
-    return ListView.builder(
-      itemBuilder: (BuildContext context, int index) => categories[index],
-      itemCount: categories.length,
+  Widget createText() {
+    return Text(
+      "Karl",
+      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
     );
   }
 
-  /// Returns a list of mock [Unit]s.
-  List<Unit> _retrieveUnitList(String categoryName) {
-    return List.generate(10, (int i) {
-      i += 1;
-      return Unit(
-        name: '$categoryName Unit $i',
-        conversion: i.toDouble(),
-      );
-    });
+  Widget createList() {
+    return Container(
+      height: 200,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: <Widget>[
+          createCard(),
+          createText(),
+          createCard()
+        ],
+      ),
+    );
+  }
+
+  Widget createCard() {
+    return SizedBox(
+        width: 150,
+        height: 150,
+        child: Card(
+          elevation: 40,
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[Text("YEET"), Text("YEETIMUS")],
+              ),
+              Text("POPULATION ME")
+            ],
+          ),
+        ));
   }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Instead of re-creating a list of Categorieas in every build(),
-    // save this as a variable inside the State object and create
-    // the list at initialization (in initState()).
-    // This way, you also don't have to pass in the list of categories to
-    // _buildCategoryWidgets()
-    final categories = <Category>[];
-
-    for (var i = 0; i < _categoryNames.length; i++) {
-      categories.add(Category(
-        name: _categoryNames[i],
-        color: _baseColors[i],
-        iconLocation: Icons.add_a_photo,
-        units: _retrieveUnitList(_categoryNames[i]),
-      ));
-    }
-
-    final listView = Container(
-      color: _backgroundColor,
-      padding: EdgeInsets.symmetric(horizontal: 8.0),
-      child: _buildCategoryWidgets(categories),
-    );
-
-    final appBar = AppBar(
-      elevation: 0,
-      title: Text(
-        'Unit Converter',
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 30.0,
-        ),
-      ),
-      centerTitle: true,
-      backgroundColor: _backgroundColor,
-    );
-
     return Scaffold(
-      appBar: appBar,
-      body: listView,
-    );
+        body: Column(
+      children: <Widget>[
+        AnimatedContainer(
+            width: 100,
+            height: 100,
+            color: Color.fromARGB(0, 150, 150, 150),
+            duration: Duration(seconds: 1),
+            curve: Curves.fastOutSlowIn,
+            margin: EdgeInsets.only(left: 15, top: 115)),
+        createText(),
+        createList()
+      ],
+    ));
   }
 }
